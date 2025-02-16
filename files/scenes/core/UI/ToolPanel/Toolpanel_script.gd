3efe253d988fc0c54d2bc8f76c075a0824fbdd76
Max9th <154016869+Max9th@users.noreply.github.com>
2025-02-16 00:03:50 -0300

    added a new subviewport in main scene to allow use of native resolution files

--- files/scenes/core/UI/ToolPanel/Toolpanel_script.gd
+++ files/scenes/core/UI/ToolPanel/Toolpanel_script.gd
@@ -17,24 +17,24 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-#func _input(event: InputEvent) -> void:
-	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and can_place_wire == true:
-		#add_wire()
-#
-#func add_wire():
-	#var wire = Wire_cb.new()
-	##wire.grid_size = Vector2(90,90)
-	#wire.position = get_global_mouse_position().snapped(wire.grid_size)
-	#where_to_place_wire.add_child(wire)
-	#wire.can_drag = true
-	#can_place_wire = false
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and can_place_wire == true:
+		add_wire()
+
+func add_wire():
+	var wire = Wire_cb.new()
+	#wire.grid_size = Vector2(90,90)
+	wire.position = get_global_mouse_position().snapped(wire.grid_size)
+	where_to_place_wire.add_child(wire)
+	wire.can_drag = true
+	can_place_wire = false
 
 
 func _on_add_wire_button_pressed() -> void:
-	#can_place_wire = true
-	var tool = AddTool.instantiate()
-	tool.enabled = true
-	tool_destination_node.add_child(tool)
+	can_place_wire = true
+	#var tool = AddTool.instantiate()
+	#tool.enabled = true
+	#tool_destination_node.add_child(tool)
 
 func _on_del_wire_button_pressed() -> void:
 	#can_place_wire = false
